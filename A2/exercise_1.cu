7fcb9c533412423fbb202fa5f3451226712d688a
Kevin_Xiong <934644014@qq.com>
2020-11-09 14:49:58 +0800

    Update exercise_1.cu

--- A2/exercise_1.cu
+++ A2/exercise_1.cu
@@ -1,13 +1,14 @@
 #include<stdio.h>
 
 __global__ void Hello (){
-    int id = blockIdx.y;
+    int id =blockIdx.x*blockDim.x+threadIdx.x;
     printf("hello from thread %d",id);
 }
 
 
 int main(){   
     Hello<<<1,256>>>();
+    cudaDeviceSynchronize();
     return 0;
 
 }
